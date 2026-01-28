5a77294fb8323ba0ccb35eda17aeaa351cbd7db1
Lehaibac05 <bac.le@software.codegym.vn>
2026-01-28 16:25:11 +0700

    demo_part3 add ememy and update player shoot

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,27 +4,28 @@ public class PlayerShoot : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public float shootingInterval = 0.2f;
-
+    public Vector3 bulletOffset;
     private float lastBulletTime;
 
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            if (Time.time - lastBulletTime > shootingInterval)
-            {
-                ShootBullet();
-                lastBulletTime = Time.time;
-            }
+            UpdateFiring();
         }
     }
 
-    void ShootBullet()
+    private void UpdateFiring()
     {
-        Instantiate(
-            bulletPrefab,
-            transform.position,
-            transform.rotation
-        );
+        if (Time.time - lastBulletTime > shootingInterval)
+        {
+            ShootBullet();
+            lastBulletTime = Time.time;
+        }
+    }
+
+    private void ShootBullet()
+    {
+        var bullet = Instantiate(bulletPrefab, transform.position + bulletOffset, transform.rotation);
     }
 }
